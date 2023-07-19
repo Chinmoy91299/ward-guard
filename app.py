20721b1ba71ed28ca2fbbba1cc55aa3e9b091f59
Chinmoy Jyoti Hazarika <72558658+Chinmoy91299@users.noreply.github.com>
2023-07-19 14:04:10 +0800

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Import the Flask class from the flask module
-from flask import Flask, render_template, Response, send_file
+from flask import Flask, render_template, Response, session, jsonify
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
@@ -10,150 +10,34 @@ import cv2
 
 # Create an instance of the Flask class
 app = Flask(__name__)
-cam0 = cv2.VideoCapture(0)
+app.secret_key = "your-secret-key"
+camera = None
 
 
-# load our serialized face detector model from disk
+# Load our serialized face detector model from disk
 prototxtPath = r"face_detector\deploy.prototxt"
 weightsPath = r"face_detector\res10_300x300_ssd_iter_140000.caffemodel"
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
-    
-# load the face mask detector model from disk
+
+# Load the face mask detector model from disk
 maskNet = load_model("mask_detector.model")
-    
-# initialize the video stream
-print("[INFO] starting video stream...")
-
-
-noOfpeople=0
-hasMask=False
-sentAlready=False
-
-
 
 
 # Register a route
 @app.route('/')
 def home():
+    if 'camera_open' in session and session['camera_open']:
+        session['camera_open'] = False
+        release_camera()
+
     return render_template('index.html')
 
 
-def gen(video):
+def gen():
+    global camera
+
     while True:
-        success, image = video.read()
-        ret, jpeg = cv2.imencode('.jpg', image)
-        frame = jpeg.tobytes()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-        
-
-
-def sendMessage():
-	global sentAlready
-	if(sentAlready):
-		print("Message already sent")
-		return
-		
-	sentAlready=True
-	acc_sid = "AC6c198a4e05e3991b1fe583c1bb5d5e26"
-	auth_program = "bf988110b7c39c73677adea121d326ee"
-	twilio_num = "+14847598067"
-	target_num="+919344994959"
-
-		
-
-	client=Client(acc_sid,auth_program)
-
-	message=client.messages.create(
-		body="Please wear a mask",
-		from_=twilio_num,
-		to=target_num
-	)
-	print(message.body)
-
-
-
-
-
-
-def detect_and_predict_mask(frame, faceNet, maskNet):
-	global noOfpeople
-	# grab the dimensions of the frame and then construct a blob
-	# from it
-	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
-		(104.0, 177.0, 123.0))
-
-	# pass the blob through the network and obtain the face detections
-	faceNet.setInput(blob)
-	detections = faceNet.forward()
-	# print(detections.shape)
-
-	# initialize our list of faces, their corresponding locations,
-	# and the list of predictions from our face mask network
-	faces = []
-	locs = []
-	preds = []
-
-	# loop over the detections
-	for i in range(0, detections.shape[2]):
-		# extract the confidence (i.e., probability) associated with
-		# the detection
-		confidence = detections[0, 0, i, 2]
-
-		# filter out weak detections by ensuring the confidence is
-		# greater than the minimum confidence
-		if confidence > 0.5:
-			# compute the (x, y)-coordinates of the bounding box for
-			# the object
-			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-			(startX, startY, endX, endY) = box.astype("int")
-
-			# ensure the bounding boxes fall within the dimensions of
-			# the frame
-			(startX, startY) = (max(0, startX), max(0, startY))
-			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
-
-			# extract the face ROI, convert it from BGR to RGB channel
-			# ordering, resize it to 224x224, and preprocess it
-			face = frame[startY:endY, startX:endX]
-			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB) 
-			face = cv2.resize(face, (224, 224))
-			face = img_to_array(face)
-			face = preprocess_input(face)
-
-			# add the face and bounding boxes to their respective
-			# lists
-			faces.append(face)
-			locs.append((startX, startY, endX, endY))
-
-	# only make a predictions if at least one face was detected
-	if len(faces) > 0:
-		# for faster inference we'll make batch predictions on *all*
-		# faces at the same time rather than one-by-one predictions
-		# in the above `for` loop
-		noOfpeople=len(faces)
-		faces = np.array(faces, dtype="float32")
-		preds = maskNet.predict(faces, batch_size=32) 
-	else:
-		noOfpeople=0
-	
-	print("No of people: ",noOfpeople)
-	# return a 2-tuple of the face locations and their corresponding
-	# locations
-	return (locs, preds)
-    
-
-
-
-
-
-
-def gener(vs):
-
-    # loop over the frames from the video stream
-    while True:
-        success,frame = vs.read()
+        success, frame = camera.read()
         frame = imutils.resize(frame, width=400)
 
         (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
@@ -163,58 +47,91 @@ def gener(vs):
             (mask, withoutMask) = pred
 
             label = "Mask" if mask > withoutMask else "No Mask"
-            hasMask= mask > withoutMask
+            hasMask = mask > withoutMask
 
             color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
 
             label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
             cv2.putText(frame, label, (startX, startY - 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
             cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
-            
-            # if(not hasMask):
-                #    sendMessage()
 
-        # cv2.imshow("Frame", frame)
-        
         ret, jpeg = cv2.imencode('.jpg', frame)
         augmentedFrame = jpeg.tobytes()
         yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + augmentedFrame + b'\r\n\r\n')
-        
 
 
+def detect_and_predict_mask(frame, faceNet, maskNet):
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
+                                 (104.0, 177.0, 123.0))
+
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
+
+    faces = []
+    locs = []
+    preds = []
+
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+
+        if confidence > 0.5:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+            face = frame[startY:endY, startX:endX]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (224, 224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
+
+            faces.append(face)
+            locs.append((startX, startY, endX, endY))
+
+    if len(faces) > 0:
+        faces = np.array(faces, dtype="float32")
+        preds = maskNet.predict(faces, batch_size=32)
+
+    return (locs, preds)
 
 
+def release_camera():
+    global camera
 
+    if camera is not None:
+        camera.release()
+        camera = None
 
-
-
-
-        
 
 @app.route('/video_feed')
 def video_feed():
-    global video
-    return Response(gener(cam0),mimetype='multipart/x-mixed-replace;boundary=frame')
+    global camera
 
+    if 'camera_open' not in session or not session['camera_open']:
+        session['camera_open'] = True
+        camera = cv2.VideoCapture(0)
 
+    return Response(gen(), mimetype='multipart/x-mixed-replace;boundary=frame')
 
-@app.route('/checker_feed')
-def checker_feed():
-    global noOfpeople
-    if(noOfpeople==0):
-          return send_file("static/ddd.jpg", mimetype='image/jpg')
-    else:
-          return send_file("static/dss.jpg", mimetype='image/jpg')
-
-
+@app.route('/get_mask_result')
+def get_mask_result():
+   
+    return jsonify({'has_mask': True})   
 
 @app.route('/ImageStream')
 def ImageStream():
-    """the live page"""
+    if 'camera_open' in session and session['camera_open']:
+        session['camera_open'] = False
+        release_camera()
+
     return render_template('RealtimeImage.html')
+
 
 # Run the Flask application
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True) 
